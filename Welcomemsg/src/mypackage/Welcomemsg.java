4a84ee61d5677e034f42307705efb67f68b2b376
manukonjeti <konjetimanisha@gmail.com>
2024-11-12 16:47:40 -0500

    Scanner class

---
+++ Welcomemsg/src/mypackage/Welcomemsg.java
@@ -0,0 +1,13 @@
+package mypackage;
+import java.util.*;
+public class Welcomemsg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Scanner sc= new Scanner(System.in);
+String name=sc.nextLine();
+System.out.println("WELCOME "+ name);
+	}
+
+}
